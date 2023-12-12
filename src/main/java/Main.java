eab1c63e8c97a7e8c09bb518fc7c7201558d11f4
huhdy32 <mong3268@gmail.com>
2023-12-12 12:32:47 +0900

    init: 백준 풀이 초기설정

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String temp = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        System.out.print(temp);
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import java.io.*;
+import java.util.stream.Stream;
+
+@DisplayName("백준 문제 테스트")
+public class MainTest {
+
+    static final InputStream ORIGINAL_INPUT_STREAM = System.in;
+    static final PrintStream ORIGINAL_OUTPUT_STREAM = System.out;
+    ByteArrayOutputStream byteArrayOutputStream;
+
+    @ParameterizedTest
+    @MethodSource("Supply_Test_Source")
+    @DisplayName("테스트 케이스 수행")
+    void Test_Main(final String data, final String result) throws IOException {
+        initInputStream(data);
+        Main.main(new String[]{});
+        Assertions.assertEquals(byteArrayOutputStream.toString(), result);
+    }
+
+    static Stream<Arguments> Supply_Test_Source() {
+        return Stream.of(
+                Arguments.of("12", "12"),
+                Arguments.of("12042", "12042")
+        );
+    }
+
+    @AfterEach
+    public void restoreInputStream() {
+        System.setIn(ORIGINAL_INPUT_STREAM);
+        System.setOut(ORIGINAL_OUTPUT_STREAM);
+    }
+
+    void initInputStream(final String data) {
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+    }
+
+    @BeforeEach
+    void setOutputStream() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+}
